01690f93ef7c3d4399fe8df44fd9f1da3a31933f
Ian Callender <iancalle@uw.edu>
2019-12-03 12:34:14 -0800

    add database branch

--- app/src/main/java/edu/uw/colind4/calendar_planner/MainActivity.kt
+++ app/src/main/java/edu/uw/colind4/calendar_planner/MainActivity.kt
@@ -1,12 +1,21 @@
 package edu.uw.colind4.calendar_planner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event_map)
+        setContentView(R.layout.activity_main)
+
+        newEventBtn.setOnClickListener {
+            val intent = Intent(this, add_event::class.java)
+            this.startActivity(intent)
+        }
     }
 }
